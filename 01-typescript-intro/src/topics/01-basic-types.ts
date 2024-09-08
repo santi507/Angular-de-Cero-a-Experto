5d769b382b21fc61c1644274d8cf5053a0d8ef46
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-09-07 22:43:58 -0500

    section #03: basic types

---
+++ 01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,12 @@
+const name: string = 'Santiago';
+
+let hpPoints: number | 'Full';
+hpPoints = 'Full';
+
+const isAlive: boolean = true;
+
+console.log({
+    name,
+    hpPoints,
+    isAlive
+});
