81aa8227426c0f0a936addbc9cdef93647b4f551
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-09-08 16:51:56 -0500

    section #03: functions
    * basic functions
    * functions with objects as parameters

---
+++ 01-typescript-intro/src/topics/03-functions.ts
@@ -0,0 +1,44 @@
+function addNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+const addNumbersArrow = (a: number, b: number): string => {
+    return `${a + b}`;
+}
+
+function multiply(firstNumber: number, secondNumber?: number, base: number = 2): number {
+    return firstNumber * base;
+}
+
+const result: number = addNumbers(1, 2);
+const result2: string = addNumbersArrow(1, 2);
+const result3: number = multiply(5);
+
+console.table({
+    result,
+    result2,
+    result3
+})
+
+interface Character {
+    name: string;
+    hp: number;
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'strider',
+    hp: 50,
+    showHp() {
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+}
+
+healCharacter(strider, 10);
+healCharacter(strider, 20);
+
+strider.showHp();
