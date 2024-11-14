0f4078a7bda40aa2c1ffd9cc34a0e5ee46fe2eea
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-11-13 21:03:39 -0500

    Homework: change name and age

--- 02-bases/src/app/heroes/hero/hero.component.ts
+++ 02-bases/src/app/heroes/hero/hero.component.ts
@@ -16,4 +16,12 @@ export class HeroComponent {
   getHeroDescription(): string {
     return `${this.name} - ${this.age}`;
   }
+
+  changeHero(): void {
+    this.name = 'Superman';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
 }
