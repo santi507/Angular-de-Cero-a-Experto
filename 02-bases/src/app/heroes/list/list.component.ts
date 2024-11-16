7b80d41d195a50ac0c99bb52ee111ef09cb087d1
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-11-15 23:29:10 -0500

    ngFor, ngIf-Else and ngTemplate

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -5,4 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
-export class ListComponent {}
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'Thor',
+    'Capitan America',
+  ];
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
+}
