3ab02cc588753b9805fd7becc2b05269e2483831
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-11-16 21:37:15 -0500

    Heroes Module

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -3,14 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CounterModule } from './counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
-import { HeroComponent } from './heroes/hero/hero.component';
-import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CounterModule, HeroesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
