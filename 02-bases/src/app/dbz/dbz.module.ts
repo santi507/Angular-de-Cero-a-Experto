896efc5d8aef97101a493d2c23259ed2ade432ee
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-11-20 21:38:24 -0500

    DBZ List Component

--- 02-bases/src/app/dbz/dbz.module.ts
+++ 02-bases/src/app/dbz/dbz.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, ListComponent],
   imports: [CommonModule],
   exports: [MainPageComponent],
 })
