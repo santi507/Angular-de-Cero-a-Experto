137891b704db8758fe2428f85a2a66ef517f7b4c
Santiago Rios Samaniego <santiago.rios1758@gmail.com>
2024-12-23 20:07:30 -0500

    ViewChild - HTML reference

--- 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
+++ 03-gifs-app/src/app/gifs/components/search-box/search-box.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {}
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+    console.log({newTag});
+    
+  }
+}
